6e50a13eae02addaaa626375709ccf234727c33c
Tim Chase <tchase4544@gmail.com>
2017-05-31 20:31:12 -0400

    controller added

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+
   root 'welcome#index'
+  
+  get '/articles/new' do
+  end
 end
